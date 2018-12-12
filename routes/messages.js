2db21558a30994c999a036c9e92dffb66d53a4ff
Jake Mosher <jakemo136@gmail.com>
2018-12-12 10:27:32 -0700

    get ONE complete and passing test

--- routes/messages.js
+++ routes/messages.js
@@ -1,10 +1,39 @@
+'use strict'
 
-'use strict';
+const knex = require('../knex.js')
+const express = require('express')
+const router = express.Router()
 
-const express = require('express');
-
-const router = express.Router();
 
 // YOUR CODE HERE
+// get ONE
+router.get('/', (req, res, next) => {
+  knex('messages')
+    .select('id', 'name', 'message')
+    .then((response) => {
+      res.status(200).send(response)
+    })
+})
 
-module.exports = router;
+// get ALL
+router.get('/:id', (req, res, next) =>{
+  
+})
+
+// add ONE
+router.put('/', (req, res, next) =>{
+  
+})
+
+// update ONE
+router.patch('/', (req, res, next) =>{
+  
+})
+
+// delete ONE
+router.delete('/', (req, res, next) =>{
+  
+})
+
+
+module.exports = router
