e3f146f75fb1992ee707c90bb9d9a698ada8ae47
Jake Mosher <jakemo136@gmail.com>
2019-01-23 09:33:16 -0700

    added CORS stuff because BLECH

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const messagesRouter = require('./routes/messages');
+var cors = require("cors")
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors())
 
 app.use('/messages', messagesRouter);
 
