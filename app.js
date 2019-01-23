9de71a39928ba797e595dd04f9da9d6ce6825ca6
Jake Mosher <jakemo136@gmail.com>
2019-01-23 09:43:54 -0700

    removed old CORS code and pasted in app.use() from assessment repo

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const messagesRouter = require('./routes/messages');
-// var cors = require("cors")
 
 const app = express();
 
@@ -13,8 +12,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PATCH,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
-// app.use(cors())
 
 app.use('/messages', messagesRouter);
 
